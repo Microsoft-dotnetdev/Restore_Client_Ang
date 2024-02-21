0c8733658fc1cab5d41f34b148a9732c06e62969
microsoft-dotnetdev <microsoft.det.dev@gmail.com>
2024-02-21 20:34:43 +0530

    End of Section 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Restore_Client_Ang';
